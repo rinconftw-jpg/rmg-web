affa33f78ffc84edcfe466179f759e46441099e2
rinconftw-jpg <doublerconstructiontx@gmail.com>
2026-05-24 17:53:41 -0500

    Make RMG and CONSTRUCTION same cream color in hero

--- app/page.tsx
+++ app/page.tsx
@@ -30,7 +30,7 @@ export default function Home() {
 
         <div className="relative z-[4] text-center px-4 max-w-4xl mx-auto">
           <h1 className="fade-up-1 text-4xl sm:text-6xl md:text-7xl lg:text-8xl font-black leading-none mb-4 tracking-tight">
-            <span className="text-white">RMG</span>{' '}
+            <span className="text-[#F4E7C9]">RMG</span>{' '}
             <span className="text-[#F4E7C9]">CONSTRUCTION</span>
           </h1>
           <p className="fade-up-2 text-gray-300 text-base sm:text-lg md:text-xl max-w-xl mx-auto mb-4 leading-relaxed">
